57ffeaa70a0ea4b03de65c236bfee3c103ecf189
custodio1jimar-coder <custodio1jimar@gmail.com>
2026-08-14 23:00:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="joans-kitchenette-continuous-v1";
+const CACHE="joans-kitchenette-scan-sound-v1";
 const ASSETS=['./','./index.html','./logo.jpg','./gcash_qr.jpg','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('joans-kitchenette-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
